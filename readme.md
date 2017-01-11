937620417c33fa3562af6d370b5ecdf6739acdd2
xubaoshi <948869516@qq.com>
2017-01-11 15:47:22 +0800

    edit resize.js

---
+++ readme.md
@@ -0,0 +1,3 @@
+# 表格拖拽 #
+## demo ##
+![](http://i.imgur.com/JwM1aR6.gif)
--- resize.js
+++ resize.js
@@ -1,15 +1,15 @@
 (function () {
     var ResizeColumns = function (settings) {
-        var options = {
+        this.options = {
             target: '',
-            store: window.localStorage,
             colMinWidth: 35,
-            noResizeClass: '.js-no-resize',
-            resizeClass: 'g-clip'
+            noResizeClass: 'js-no-resize',
+            resizeClass: 'g-clip',
+            columnName: 'resize-column-name'
         }
-        $.extend(options, settings);
+        $.extend(this.options, settings);
 
-        if (options.target == '') {
+        if (this.options.target == '') {
             console.error('please set resize tables!')
             return;
         } else {
@@ -21,6 +21,18 @@
     $.extend(ResizeColumns.prototype, {
         init: function ($table) {
             var _this = this;
+            var $ths = $table.find('th:not(.' + _this.options.noResizeClass + '):visible');
+            var $tbodyTrs = $table.find('tbody').find('tr');
+            if ($tbodyTrs.length > 0 && $ths.length > 0) {
+                $ths.splice(0, 1);
+                $ths.addClass(_this.options.resizeClass);
+                $tbodyTrs.each(function () {
+                    var $tds = $(this).find('td:not(.' + _this.options.noResizeClass + '):visible');
+                    $tds.splice(0, 1);
+                    $tds.addClass(_this.options.resizeClass);
+                })
+            }
+
             _this.$table = $table;
             _this.tableId = this.$table.data('resize-columns-id');
             _this.mousedown = this.bind(this.mousedown, this);
@@ -32,18 +44,24 @@
             }))
             return _this;
         },
+        /**
+         * 销毁拖拽区域
+         */
         destroy: function () {
             this.$handleContainer.remove();
-            this.$table, removeData('resizeColumns');
+            this.$table.removeData('resizeColumns');
             this.$table.css({ 'width': 'auto' });
-            this.$table.find('tr th').filter('.g-clip').css({ 'width': '', 'min-width': '' });
+            this.$table.find('tr th').filter('.' + this.options.resizeClass).css({ 'width': '', 'min-width': '' });
             return $(window).off('.rc');
 
         },
+        /**
+         * 创建拖拽区域
+         */
         createHandles: function () {
             var _this = this;
             _this.$table.before((_this.$handleContainer = $('<div class="rc-handle-container"></div>')));
-            this.$table.find('tr th').filter('.g-clip').each(function (i, el) {
+            this.$table.find('tr th').filter('.' + this.options.resizeClass).each(function (i, el) {
                 var $handle = $('<div class="rc-handle"></div>');
                 $handle.data('th', $(el));
                 return $handle.appendTo(_this.$handleContainer);
@@ -51,11 +69,14 @@
             _this.$table.width(10 + 'px');
             return _this.$handleContainer.on('mousedown', '.rc-handle', _this.mousedown);
         },
+        /**
+         * 鼠标点击事件
+         */
         mousedown: function (e) {
             e.preventDefault();
             var _this = this;
             var $table = _this.$table;
-            var tableWidth = $table.width() - 1; // table宽度获取的时候减去1px宽度，否则拖动时会抖动
+            var tableWidth = $table.width() - 1;  // table宽度获取的时候减去1px宽度，否则拖动时会抖动
             var $currentGrip = $(e.currentTarget);
             var $leftColumn = $currentGrip.data('th');
             var leftColumnStartWidth = $leftColumn.width();
@@ -75,11 +96,14 @@
             });
 
             return $(document).one('mouseup', function () {
-                $(document).off('mousedown.rc');
+                $(document).off('mousemove.rc');
                 return _this.saveColumnWidths();
             })
 
         },
+        /**
+         * 同步拖拽区域
+         */
         syncHandleWidths: function () {
             var _this = this;
             _this.$handleContainer.width(_this.$table.width());
@@ -90,17 +114,75 @@
                 })
             })
         },
+        /**
+         * 暂存拖拽信息
+         */
         saveColumnWidths: function () {
-
+            var _this = this;
+            var storage = window.localStorage;
+            var key = '', value = {}, valueJSON = '';
+            var path = location.pathname;
+            if (_this.isLocalStorage()) {
+                key = path + '-' + _this.$table.attr('id');
+                _this.$table.find('tr th').filter('.' + _this.options.resizeClass).each(function (_, el) {
+                    var columnName = $(el).attr(_this.options.columnName);
+                    if (columnName != '') {
+                        value[columnName] = $(el).width();
+                    } else {
+                        console.log('表格th属性' + _this.options.columnName + '为空！')
+                        return true;
+                    }
+                })
+                if (value) {
+                    valueJSON = JSON.stringify(value);
+                    storage.setItem(key, valueJSON);
+                }
+            }
         },
+        /**
+         * 根据暂存数据同步拖拽位置
+         */
         restoreColumnWidths: function () {
-
+            var _this = this;
+            var storage = window.localStorage;
+            var key = '', value = {}, valueJSON = '';
+            var path = location.pathname;
+            if (_this.isLocalStorage()) {
+                key = path + '-' + _this.$table.attr('id');
+                valueJSON = storage.getItem(key);
+                value = JSON.parse(valueJSON);
+                _this.$table.find('tr th').filter('.' + _this.options.resizeClass).each(function (_, el) {
+                    var columnName = $(el).attr(_this.options.columnName);
+                    if (value && columnName != '' && columnName != undefined && value[columnName]) {
+                        $(el).css({ 'width': value[columnName] + 'px' });
+                    } else {
+                        var iMinWidth = parseInt($(el).css('min-width'));
+                        iMinWidth = iMinWidth == 0 ? _this.options.colMinWidth : iMinWidth;
+                        $(el).css({ 'width': iMinWidth + 'px' });
+                    }
+                    // 移除min-width IE下拖拽不能小于min-width
+                    $(el).css({ 'min-width': 'auto' });
+                })
+            }
+        },
+        isLocalStorage: function () {
+            var testKey = 'test', testValue = 'testValue';
+            var storage = window.localStorage;
+            try {
+                storage.setItem(testKey, testValue);
+                storage.removeItem(testKey);
+                return true;
+            } catch (error) {
+                console.log('当前浏览器不支持localStorage');
+                return false;
+            }
         },
         bind: function (fn, me) {
             return function () {
                 return fn.apply(me, arguments);
             }
         }
-
     })
+
+    window.ResizeColumns = ResizeColumns;
 })();
